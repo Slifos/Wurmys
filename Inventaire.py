6b77cdc557e345318b826166dbfccd8ba631469a
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2024-03-24 16:49:57 +0100

    inventaire

--- Inventaire.py
+++ Inventaire.py
@@ -10,8 +10,8 @@ WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Inventaire")
 screen = pygame.display.set_mode((1080, 720))
 
-background = pygame.image.load('images/inventaire_wurmys.png')
-background=pygame.transform.scale(background,[1270,780])
+background = pygame.image.load('images/inventaire.png')
+background=pygame.transform.scale(background,[1080,720])
 #charger le jeu
 
 
@@ -22,16 +22,17 @@ BLACK = (0, 0, 0)
 
 # Définition des éléments de l'inventaire
 inventory_items = {
-    "sword": pygame.image.load("images/Worms.png"),
-    "potion": pygame.image.load("images/maps.png"),
+    "gun": pygame.image.load("images/pistolet.png"),
+    "grenad":pygame.image.load("images/Grenade.png"),
     # Ajoutez d'autres éléments ici
 }
-
+inventory_items["grenad"]=pygame.transform.scale(inventory_items["grenad"],[200,120])
 # Position de départ de l'inventaire
-inventory_x = 50
-inventory_y = 50
+inventory_x = 27
+inventory_y = 75
 
 # Boucle principale
+items_position={}
 while True:
     WINDOW.fill(WHITE)
     screen.blit(background, (0, 0))
@@ -39,7 +40,9 @@ while True:
     x, y = inventory_x, inventory_y
     for item_name, item_image in inventory_items.items():
         WINDOW.blit(item_image, (x, y))
-        x += item_image.get_width() + 10
+        items_position[item_name]=x,y
+        x+=150
+        print(items_position[item_name])
 
     # Gestion des événements
     for event in pygame.event.get():
