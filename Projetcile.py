a9fa4c9d7cf9ba808c0ede793617149155e61057
Tonyvara8 <151458140+Tonyvara8@users.noreply.github.com>
2024-04-06 19:09:06 +0200

    projectile

---
+++ Projetcile.py
@@ -0,0 +1,79 @@
+import pygame
+from math import *
+
+
+
+
+class Projectile(pygame.sprite.Sprite) :
+    def __init__(self,x_pos,z_pos,radius,mass,retention,color,x_speed,z_speed,id):
+        super().__init__()
+        self.x_pos=x_pos
+        self.z_pos=z_pos
+        self.radius=radius
+        self.color=color
+        self.mass=mass
+        self.retention=retention
+        self.x_speed=x_speed
+        self.z_speed=z_speed
+        self.id=id
+        self.circle= ''
+        self.angle=3.141592/4
+    def get_angle(self,x,y):
+
+        angle=atan(y/x)
+        return angle 
+    def draw(self,screen):
+
+        self.circle=pygame.draw.circle(screen,self.color,(self.x_pos,self.z_pos),self.radius)
+    
+    def lancement(self,screen,seconde,pos_d):
+        sol=1700
+        
+        
+
+        #self.z_speed=-9.81*seconde+sin(3.141592/6)*220
+        self.z_pos=((-9.81/2)*(seconde**2)+(sin(self.angle)*100)*seconde+pos_d)*-1+1000
+        self.x_pos=(cos(self.angle)*100)*seconde+500
+        self.draw(screen)
+        print("Z :" ,self.z_pos)
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def vitesse_chute_libre(self,seconde):
+            self.z_speed=9.81*seconde
+
+
+    def gravity(self,largeur,seconde):
+        bounce_stop=0.2
+        if self.z_pos< largeur -self.radius :
+            self.vitesse_chute_libre(seconde)
+        else:
+            print("SoOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOL")
+            if self.z_speed> bounce_stop:
+                self.z_speed=self.z_speed * (-1)* self.retention
+
+
+            else:
+                if abs(self.z_speed) <= bounce_stop:
+                    self.z_speed=0
+        print(self.z_speed)
+        return self.z_speed
+    def update(self):
+        self.z_pos+=self.z_speed
+        self.x_pos+=self.x_speed
+
+   
+        
--- jeu.py
+++ jeu.py
@@ -1,8 +1,10 @@
 import pygame
-from Player import player
+from player import player
+from Inventaire import Inventaire
+from Projetcile import *
 pygame.init()
 font= pygame.font.SysFont("LEMONMILK",40)
-ecran = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 Text_color =(255,255,255)
 bgjeu = pygame.image.load("images/maps.png").convert()
 bgjeu = pygame.transform.scale(bgjeu, [1920,1080])
@@ -11,18 +13,67 @@ player= player()
 continuer = True
 def draw_texte (texte,font,text_color,x,y):
     img=font.render(texte,True,text_color)
-    ecran.blit(img,(x,y))
+    screen.blit(img,(x,y))
 
+
+
+Largeur,Hauteur=pygame.display.get_surface().get_size()
+
+ball=Projectile(500,1000,10,5,.8,'green',0,0,1)
+pos_d=500
+inventair=Inventaire()
+fpsClock=pygame.time.Clock()
 while continuer:    
     draw_texte("Quitter jeu", font, Text_color, 1000, 700)
-    ecran.blit(bgjeu, (0,0 ))
+    screen.blit(bgjeu, (0,0 ))
     # ici on crée un rectangle de couleur rose, en x=0, y=0 et de taille 300 sur 200
-    # nous verrons plus tard comment faire plus en détail :)
+    # nous verrons plus tard comment faire plus en détail :g)
     #faire apparaitre joueur
-    ecran.blit(player.image,player.rect)
+    screen.blit(player.image,player.rect)
+    
+    
+    temps=0
+    seconde=0
+    pygame.mouse.set_visible(1)
+    
     for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            continuer = False
+        if event.type == pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                continuer = False
+        if event.type== pygame.KEYDOWN:
+            if event.key==pygame.K_g:
+                inventair.inven(True, screen)
+                print("oui")
+        if event.type== pygame.KEYDOWN:
+            if event.key==pygame.K_z:
+                
+                buton_clicked=0
+                
+                while  buton_clicked==0:
+                    event2= pygame.event.poll()
+                        
+                    if event2.type == pygame.MOUSEBUTTONDOWN:
+                                
+                        print("oui")  
+                                
+                        x,y =pygame.mouse.get_pos()
+                        ball.angle=ball.get_angle(x,y)
+                        print(x,y)
+                        buton_clicked=1                       
+                    while ball.z_pos<1700:
+                        print(seconde)
+                        print("45")               
+                        print("angle",ball.angle)  
+
+                        ball.lancement(screen,seconde,pos_d)
+                        seconde=(pygame.time.get_ticks() - temps) /1000
+                        pygame.display.flip()
+                       
+
+                   
+
+                
+    #fpsClock.tick(60)
     # ici on actualise l'écran, car on a affiché un rectangle rose, et on veut qu'il soit
     # visible. Si l'on avait pas mit cette instruction, on n'aurait jamais vu le rectangle !
     pygame.display.flip()
