bbce02a2e6b47959ae28b8becd209270c532244f
Slifos <97668299+Slifos@users.noreply.github.com>
2024-05-05 17:33:05 +0200

    Update gameplay.py

--- gameplay.py
+++ gameplay.py
@@ -10,17 +10,20 @@ from Pause import pause
 pygame.init()
 def gameplay():
 
-    screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
-    w,h = pygame.display.get_surface().get_size()
+    #initialise le jeu
+    w, h = pygame.display.Info().current_w, pygame.display.Info().current_h
+    screen = pygame.display.set_mode((w, h), pygame.RESIZABLE)
     background = pygame.image.load('bg/bg_game.jpg')
-    background = pygame.transform.scale(background, (w,h))
-    # charger le jeu
-    player1 = Player("A")
-    player1.update_setting(w,h)
-    K_player1 = {"left": pygame.K_q, "right": pygame.K_d, "up": pygame.K_z}
+    background = pygame.transform.scale(background, (w,h))#initialise le fond en fonction des dimensions de l'écran (16:9 de préférence)
+    play_music("sound/game_theme.mp3")#joue le thème du jeu
 
-    mode = 2
-    play_music("sound/game_theme.mp3")
+    # initialise les joueurs
+    player1 = Player("A",w,h)
+    player1.update_setting(w,h)#permets aux joueurs de changer ses settings
+    #initialise les touches du joueur1
+    K_player1 = {"left": pygame.K_q, "right": pygame.K_d, "up": pygame.K_z}
+    mode = 2 #lancement du jeu
+
     inventaire = Inventaire()
     fps= pygame.time.Clock()
     pygame.mouse.set_visible(False)
@@ -32,35 +35,43 @@ def gameplay():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_BACKSPACE:
                     mode = pause(screen)
-        seconde = 0
         keys = pygame.key.get_pressed()
 
 
 
-        screen.blit(background, (0, 0))
-        if keys[K_player1["left"]]:
+
+        #déplacement du joueur 1
+        if keys[K_player1["left"]]:#détecte si il va à gauche
             player1.move_left()
-        if keys[K_player1["right"]]:
+        if keys[K_player1["right"]]:#détecte si il va à droite
             player1.move_right()
-        if keys[K_player1["up"]]:
+        if keys[K_player1["up"]]:#détecte le jump
             player1.do_jump()
-        player1.in_aire()
-        screen.blit(player1.image, (player1.rect_x,player1.rect_y) ) #(player1.rect_x,player1.rect_y)
-
-        # vérifier où le joueur veut aller
-
-
-        # mettre à jour l'écran
-        pygame.display.flip()
+        player1.in_aire() #calcule sa trajectoire si il est dans les aires
+        #implémente ces différentes actions sur l'écran
 
 
 
 
 
 
+
+
+
+
+        #optimise  le nombre d'images par seconde
         fps.tick(60)
+        #mets à jour l'écran
+        screen.blit(background, (0, 0))
+        screen.blit(player1.image, (player1.rect_x, player1.rect_y))
+        player1.update_health_bar( screen)
+
+
         pygame.display.flip()
+
+
     if mode == -1: #restart le jeu avec un appel réccursif
         mode=gameplay()
     return mode
 
+gameplay()
