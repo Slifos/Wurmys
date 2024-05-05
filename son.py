628fe7f6aa5015fe04de637b994361629ca44121
Slifos <97668299+Slifos@users.noreply.github.com>
2024-05-05 17:34:45 +0200

    Update son.py

--- son.py
+++ son.py
@@ -7,11 +7,12 @@ def play_music(file_path):
     pygame.mixer.init()
     pygame.mixer.music.stop()
     pygame.mixer.music.load(file_path)
+    pygame.mixer.music.set_volume(0.3)
     pygame.mixer.music.play(-1)
 
 def play_sound(file_path):
     pygame.init()
     pygame.mixer.init()
-    pygame.mixer.music.stop()
-    pygame.mixer.music.load(file_path)
-    pygame.mixer.music.play()
+    sound = pygame.mixer.Sound(file_path)
+    sound.set_volume(0.5)
+    sound.play()
