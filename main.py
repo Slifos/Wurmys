3642bb50aa4eb04b426a9b5200184b20ae793888
Gaspard-M <151040265+Gaspard-M@users.noreply.github.com>
2024-05-19 13:37:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,31 @@ import pygame
 from menu import menu
 from gameplay import gameplay
 from option import Option
-from pygame.locals import*
-
-import sys
+from fonction import*
 
 
+
+
+"""fichier qui permet de lancer le jeu"""
 
 pygame.init()
 #fenetre du jeux
 w, h = pygame.display.Info().current_w,pygame.display.Info().current_h
-pygame.display.set_caption("Wurmys le jeu")
+pygame.display.set_caption("Wurmys: The battle of worms")
 logo = pygame.image.load('ui/logo.png')
 pygame.display.set_icon(logo)
 screen = pygame.display.set_mode((w,h),pygame.RESIZABLE)
 mode = 1 # défini le stade du jeu (quitter le jeu = 0, Menu = 1, Jeu = 2)
 option = Option()
+play_music("sound/theme.mp3", option)
 while mode>0:
+    """interchange entre le menu et le jeu en fonction des inputs du joueur"""
     if mode == 1:
-
         mode=menu(option)
 
-    if mode == 2:
+    if mode == 10:
         mode=gameplay(option)
+        play_music("sound/theme.mp3", option)
 
 
 
