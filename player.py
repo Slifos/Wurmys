de24abfd5f13c22dba394b49ad1069f7e8c610c4
Gaspard-M <151040265+Gaspard-M@users.noreply.github.com>
2024-05-19 13:38:38 +0200

    Update player.py

--- player.py
+++ player.py
@@ -3,57 +3,66 @@ from fonction import*
 from math import *
 from Projectile import*
 #Representation du joueur
-
+"""fichier du joueur et de chacunes de ses propriétés (PV, attack, mouvement)"""
 class Player(pygame.sprite.Sprite):
 
-    def __init__(self,skin,position,option):
-        """skin = choix de l'apparence du ver, w,h = position x,y de départ du ver"""
+    def __init__(self,position,option,id):
+        """position initial, option du jeu, id du joueur"""
         super().__init__()
         self.option = option
-        self.screen = pygame.display.get_surface()
-        self.w, self.h = self.screen.get_width(), self.screen.get_height()
+        self.screen = option.screen
+        self.id = id
+
+
+        self.alive = True
         self.health = 100
         self.max_health = 100
 
-        if skin == "A":
-            self.idle = "perso/worm_idle_A.png"
-            self.death = "perso/worm_walking_A.png"
-        if skin == "B":
-            self.idle = "perso/worm_idle_B.png"
-            self.death = "perso/worm_walking_B.png"
 
-        self.worm = Image(self.idle, self.w * 0.035, (position))
-        self.alive = True
-        self.sensi = 0.14
+        self.skin_init(option.joueurs[id].skin)
+        self.worm = Image(self.idle, self.option.w * 0.035, (position))
+        self.meter = self.worm.image.get_height()
+        self.floor = self.option.h * 0.7
+        self.rayon_viser = self.option.w * 0.044
+
+        self.seconde = 0.15
         self.direction =-1
-        self.attacking = False
-        self.isjumping = False
-
-        self.T_fleche = []
-        self.hit=0
-        self.cd_attacking = 0
+        self.vitesse = self.meter*self.seconde/3
+        self.vitesse_vol = 0
+        self.viser_x,self.viser_y = 0,0
         self.viser = 0
-        self.viser_x = 0
-        self.viser_y = 0
-        self.vitesse = self.w * 0.0035
-        self.L_projectile = []
+        self.sensi = self.option.w*0.000045
+        self.hit = 0
+        self.pc = False
+        self.T_fleche = []
 
-        if position[0]>self.w*0.5:
+
+        if position[0]>self.option.w*0.5:
             self.worm.flip()
             self.direction = 1
         self.falling = False
         self.arme_init()
+
+    def skin_init(self,skin):
+        """donne une apparence au ver"""
+        if skin == "thug":
+            self.idle = "skin/worm_idle_thug.png"
+        elif skin == "masque":
+            self.idle = "skin/worm_idle_masque.png"
+        elif skin == "chad":
+            self.idle = "skin/worm_idle_chad.png"
     def arme_init(self):
+        """établie les setting par défauts de l'épée et de l'arc"""
         self.tir = False
         self.attacking = False
         self.flip = False
-        self.arc = Image("objet/bow.png", self.w * 0.02, (self.worm.rect.x, self.worm.rect.y))
+        self.arc = Image("objet/bow.png", self.option.w * 0.02, (self.worm.rect.x, self.worm.rect.y))
         self.arc_degats = 20
         self.tir_arc = False
         self.s_bow = 10
 
-        self.epee = Image("objet/epee2.png", self.w * 0.05, (self.worm.rect.x, self.worm.rect.y))
-        self.slash = Image("objet/slash.png",self.w*0.06,(0,0))
+        self.epee = Image("objet/epee2.png", self.option.w * 0.05, (self.worm.rect.x, self.worm.rect.y))
+        self.slash = Image("objet/slash.png",self.option.w*0.06,(0,0))
         self.epee_degats = 25
         self.s_epee = 10
         self.tir_epee = False
@@ -61,14 +70,12 @@ class Player(pygame.sprite.Sprite):
 
         self.s_switch =10
         self.arme_image =self.epee.image
-        self.arme = 1
+        self.arme = 0
         self.degats = self.epee_degats
         self.s_switch = 10
 
     def arme_switch(self):
-
-
-
+        """permet d'interchanger entre 2 armes (épée, arc)"""
         if self.switching==False:
             self.s_switch = 0
             self.switching = True
@@ -81,16 +88,17 @@ class Player(pygame.sprite.Sprite):
                 self.arme_image = self.arc.image
                 self.degats = self.arc_degats
 
-    def attack_epee(self, T_joueur):
+    def attack_epee(self):
         """récupère l'endroit viser par l'arme le joueur qui attaque
-        et un tableau des joueurs et leur coordonnées pour voir si ils recoivent les damages"""
+        et un tableau des joueurs et leur coordonnées pour voir si ils recoivent les damages
+        si l'image du slash touche l'image d'un autre joueur alors il recoit des dégats"""
         hit = 0
         if self.tir_epee==False:
             #couldown avant de pouvoir réattaquer
 
             self.tir_epee=True
             self.s_epee = 0
-            for joueur in T_joueur:
+            for joueur in self.T_joueur:
                 if  joueur != self:
                     self.slash.go_center(self.viser_x,self.viser_y)
                     slash_hit = self.slash.image
@@ -98,46 +106,52 @@ class Player(pygame.sprite.Sprite):
                     self.screen.blit(slash_hit,(self.slash.rect))
                     if self.slash.rect.colliderect(joueur.worm.rect):
                         joueur.take_damage(self.degats)
-                        play_sound("sound/slash_hit.mp3",0.5,self.option.volume)
+                        play_sound("sound/slash_hit.mp3",self.option)
                         hit =1
             if hit == 0:
-                play_sound("sound/slash.mp3",0.5,self.option.volume)
+                play_sound("sound/slash.mp3",self.option)
 
-    def attack_arc(self,T_joueur):
+    def attack_arc(self):
+        """ lance un projectile temporaire qui vérifie si il est en collision avec tous les joueurs sur le terrain
+        sauf le lanceur, si le projectile touche l'image d un autre joueur alors le projectile disparait pour laisser des dégats"""
         if self.tir_arc ==False:
             self.tir_arc=True
             self.s_bow=0
-            play_sound("sound/bow_shot.mp3",0.5,self.option.volume)
+            play_sound("sound/bow_shot.mp3",self.option)
             self.vitesse_fleche_x = self.viser_x - self.worm.get_center()[0]
             self.vitesse_fleche_y = self.viser_y - self.worm.get_center()[1]
-            print(self.viser_x,self.worm.get_center()[0], self.worm.rect.x+self.worm.get_center()[0] , self.worm.rect.y+self.worm.get_center()[1])
-            tir = Fleche(self,self.vitesse_fleche_x,self.vitesse_fleche_y,self.viser_x,self.viser_y,T_joueur)
+            tir = Fleche(self,self.vitesse_fleche_x,self.vitesse_fleche_y,self.viser_x,self.viser_y,self.T_joueur)
             self.T_fleche.append(tir)
 
     def cd_bow(self, max):
+        """ couldown/ temps de récupération du tir à l'arc"""
         if self.s_bow<max:
             self.s_bow+=0.02
         else:
             self.tir_arc = False
     def cd_epee(self, max):
+        """ couldown/ temps de récupération du coup d'épée"""
         if self.s_epee<max:
             self.s_epee+=0.02
         else:
             self.tir_epee = False
     def cd_switch(self,max):
+        """ couldown/ temps de récupération du changement entre 2 armes"""
         if self.s_switch<max:
             self.s_switch+=0.02
         else:
             self.switching = False
 
 
-    def attack(self,T_joueur):
+    def attack(self):
+        """ tire ou fait un coup d'épée en fonction de l'arme utilisée"""
         if self.arme % 2 == 1:
 
-            self.attack_epee(T_joueur)
+            self.attack_epee()
         else:
-            self.attack_arc(T_joueur)
+            self.attack_arc()
     def affichage_arme(self):
+        """ affiche l'arme équipé, elle doit pointer dans la direction du curseur du joueur"""
 
         self.dx = self.viser_x - self.worm.get_center()[0]
         self.dy = self.viser_y - self.worm.get_center()[1]
@@ -162,156 +176,198 @@ class Player(pygame.sprite.Sprite):
 
 
         """===============================================================ACTION================================================================================="""
+
+
+    def move(self,x):
+        """permet de se déplacer à la manette"""
+        self.x_avant = self.worm.rect.x
+        self.vitesse_vol += self.vitesse*x/4
+
+        if not self.falling :
+            self.worm.rect.x += self.vitesse*x
+            self.vitesse_x = self.meter*x*1.5
+            self.sens =-1#sert au saut
+            if self.x_avant != self.worm.rect.x:
+                self.worm.rect.y = self.worm.rect.y-self.option.h*0.00075*sin(self.worm.rect.x+7)
+
+        if self.direction==-1 and x<0:
+            self.direction=1
+            self.worm.flip()
+        if self.direction==1 and x>0:
+            self.direction=-1
+            self.worm.flip()
     def move_right(self):
+        """permet de se déplacer à droite sur clavier"""
         if not self.falling:
             self.worm.rect.x += self.vitesse
-            self.vitesse_x = self.vitesse/5
+            self.vitesse_x = self.meter*1.5
             self.sens = 1#sert pour le saut
             self.worm.rect.y = self.worm.rect.y + self.h * 0.001 * sin(self.worm.rect.x)
-        self.vitesse_vol = self.vitesse/5
+        self.vitesse_vol = self.vitesse/4
         if self.direction==1:
             self.direction=-1
             self.worm.flip()
     def move_left(self):
+        """permet de se déplacer à gauche sur clavier"""
         if not self.falling :
             self.worm.rect.x -= self.vitesse
-            self.vitesse_x = -self.vitesse/5
+            self.vitesse_x = -self.meter*1.5
             self.sens =-1#sert au saut
             self.worm.rect.y = self.worm.rect.y-self.h*0.001*sin(self.worm.rect.x+7)
-        self.vitesse_vol = -self.vitesse / 5
+        self.vitesse_vol = -self.vitesse/4
         if self.direction==-1:
             self.direction=1
             self.worm.flip()
-
-    def aim_left(self):
-        self.viser -=self.sensi
-    def aim_right(self):
-        self.viser +=self.sensi
-    def take_damage(self,damage):
-
-        self.health -=damage
-        if self.health<=0:
-            self.health = 0
-            self.worm.switch_image(self.death,self.w*0.2)
-            self.alive = False
-            self.dodge = True
-            play_sound("sound/death.mp3",0.5,self.option.volume)
-            play_sound("sound/victory_effect.mp3",0.5,self.option.volume)
-        else:
-            if self.hit%3==0:
-
-                play_sound("sound/get_hit.mp3",0.5,self.option.volume)
-            elif self.hit%3==1:
-                play_sound("sound/get_hit2.mp3",0.5,self.option.volume)
-            else:
-                play_sound("sound/get_hit3.mp3",0.5,self.option.volume)
-            self.hit+=1
-
-
-    def go_down(self):
-        self.g = self.g + 0.0007*self.h
     def do_jump(self):
         """Action de sauté en fonction des inputs de vitesses (gauche, droite)"""
         #détecte si il peut sauter (ex: tu ne peux pas sauter dans les aires)
         if not self.falling:
-            self.vitesse_y = self.vitesse*1.3 #vitesse du saut
-            self.t = 1 #génération artificielle du temps
+
+            self.vitesse_y = self.meter*7#vitesse du saut
+            self.t = 0.02 #génération artificielle du temps
             #génére le commencement du saut
-            self.worm.rect.x += (self.vitesse_x+self.vitesse_vol) * self.t
-            self.worm.rect.y -= self.vitesse_y * self.t - (1 / 2) * self.g * self.t * self.t
+            self.v0x = self.worm.rect.x
+            self.v0y = self.worm.rect.y
+            self.worm.rect.x = self.v0x+(self.vitesse_x) * self.t
+            self.worm.rect.y = self.v0y-self.vitesse_y * self.t + (1 / 2) * self.g * self.t * self.t
             #changement d'état après le saut
             self.falling = True
-            self.isjumping = True
-
-
-
-    """============================================Mise à jour constante du jeu ================================================================================================="""
-
-    def update(self,T_joueur):
-        """Permets de maj les vecteurs vitesses en fonction de si le joueur bouge ou non
-        cela sert pour les trajectoires de saut"""
-
-        self.rayon = self.w * 0.044
-        self.viser_x = self.worm.get_center()[0] + cos(self.viser) * self.rayon
-        self.viser_y = self.worm.get_center()[1] + sin(self.viser) * self.rayon
-        self.g = self.h * 0.0025
-        self.vitesse_vol = 0
-        self.cd_bow(1.2)
-        self.cd_epee(1)
-        self.cd_switch(0.3)
-        if not self.alive:
-            self.worm.rect.y = self.worm.rect.y - self.h * 0.8
-        if not self.falling:
-            self.vitesse_x = 0
-            self.vitesse_y = 0
-
-
-
     def in_aire(self):
         """Fonction qui détecte se qu il se passe en état de chute
         cette fonction est lié au saut"""
         if self.falling:
             #fonction des lois des mouvement de newton si il détecte que l'on tombe
-            self.worm.rect.x += (self.vitesse_x+self.vitesse_vol)*self.t
-            self.worm.rect.y -= self.vitesse_y*self.t -(1/2)*self.g*self.t*self.t
-            self.t+= self.vitesse*0.03
+            self.worm.rect.x = self.v0x +(self.vitesse_x+self.vitesse_vol)*self.t
+            self.worm.rect.y = self.v0y -self.vitesse_y*self.t +(1/2)*self.g*self.t*self.t
+
+            self.t+= 0.02
+    def aim(self,x,y):
+        """permet de viser à la manette en fonction du joystick droit"""
+
+        self.viser_x = self.worm.get_center()[0] + x * self.rayon_viser
+        self.viser_y = self.worm.get_center()[1] + y * self.rayon_viser
+    def aim_left(self):
+        """permet de viser à gauche en cercle autour du joueur avec le clavier"""
+        self.pc = True
+        self.viser -=self.sensi
+    def aim_right(self):
+        """permet de viser à droite en cercle autour du joueur avec le clavier"""
+        self.pc = True
+        self.viser +=self.sensi
+    
+    def take_damage(self,damage):
+        """le joueur recoit des dégats"""
+
+        self.health -=damage
+        if self.health<=0:
+            self.health = 0
+            self.alive = False
+
+
+        else:
+            if self.hit%3==0:
+
+                play_sound("sound/get_hit.mp3",self.option)
+            elif self.hit%3==1:
+                play_sound("sound/get_hit2.mp3",self.option)
+            else:
+                play_sound("sound/get_hit3.mp3",self.option)
+            self.hit+=1
 
 
 
-    def collision(self,T_platform):
-        """calcule si le joueur respecte bien les collision
+
+
+
+    """============================================Mise à jour constante du jeu ================================================================================================="""
+
+    def update(self,option):
+        """Mets à jour l'environment du joueur à chaque boucle tel que son curseur (PC), son image, ses collisions et son statut en jeu
+        /!\ fonction super importante dans le fonctionnement du joueur"""
+
+        self.option = option
+        self.T_platform = option.T_platform
+        self.T_joueur = option.T_joueurs
+
+        if self.pc:
+            self.viser_x = self.worm.get_center()[0] + cos(self.viser) * self.rayon_viser
+            self.viser_y = self.worm.get_center()[1] + sin(self.viser) * self.rayon_viser
+        self.g = self.meter*9.81
+        self.w = option.w
+        self.h= option.h
+
+        self.cd_bow(1.2)
+        self.cd_epee(1)
+        self.cd_switch(0.3)
+        if not self.falling:
+            self.vitesse_y=0
+            self.vitesse_vol = 0
+        self.in_aire()
+        self.collision()
+        self.screen.blit(self.worm.image, (self.worm.rect.x, self.worm.rect.y))
+        self.affichage_arme()
+        self.update_health_bar()
+        self.cursor()
+        if self.falling==False:
+            self.vitesse_x = 0
+
+        i = 0
+        while i < len(self.T_fleche):
+            self.T_fleche[i].update(self.option)
+            if self.T_fleche[i].t > 3:
+                del (self.T_fleche[i])
+            else:
+                i += 1
+
+
+
+
+
+
+
+
+    def collision(self):
+        """calcule si le joueur respecte bien les collision entre les platforms, le sol et les cotés
                 sinon sa position ne change pas"""
-        for platform in T_platform:
-
-            if self.worm.rect.colliderect(platform.platform_rect) and self.worm.rect.bottom > platform.platform_rect.top and self.worm.rect.top<platform.platform_rect.bottom:
-                self.falling=False
+        for platform in self.T_platform:
+            if self.worm.rect.colliderect(platform.platform_rect) and self.worm.rect.bottom >= platform.platform_rect.top and self.worm.rect.bottom < platform.platform_rect.top * self.h * 0.00106:  # and self.worm.rect.top<platform.platform_rect.bottom:
+                self.falling = False
                 break
-
-
             else:
                 if self.falling==False:
-
+                    self.falling= True
                     self.t = 0
-                    self.falling=True
-
-
+                    self.v0x = self.worm.rect.x
+                    self.v0y = self.worm.rect.y
         #collision avec les bords de map
         if self.worm.rect.x <0:
-            self.worm.rect.x = 0
-        if self.worm.rect.x >self.w*0.97:
-            self.worm.rect.x=self.w*0.97
+            self.worm.rect.x = self.option.w*0.97
+            self.v0x = self.worm.rect.x-(self.vitesse_x)*self.t
+        elif self.worm.rect.x >self.option.w*0.97:
+            self.worm.rect.x=0
+            self.v0x = self.worm.rect.x-(self.vitesse_x)*self.t
+        if self.worm.rect.y > self.floor:
+            self.falling = False
+            if self.worm.rect.y>self.floor+self.option.h*0.05:
+                self.worm.rect.y = self.floor
 
 
     """============================================AFFICHAGE==============================================================="""
 
 
 
-
     def update_health_bar(self):
-        """affichage de la barre de vie"""
-        pygame.draw.line(self.screen, (150, 0, 0), (self.worm.rect.x-self.w*0.02, self.worm.rect.y-self.h*0.05),
-                         (self.worm.rect.x-self.w*0.02+self.w*0.08, self.worm.rect.y - self.h*0.05), int(self.h*0.02))
-        pygame.draw.line(self.screen, (0, 150, 0), (self.worm.rect.x - self.w * 0.02, self.worm.rect.y - self.h * 0.05),
-                         (self.worm.rect.x-self.w*0.02 + self.w*0.08*(self.health/self.max_health), self.worm.rect.y - self.h * 0.05), int(self.h * 0.02))
-
-
-
+        """affichage de la barre de vie au dessus du joueur"""
+        pygame.draw.line(self.screen, (150, 0, 0), (self.worm.rect.x-self.option.w*0.02, self.worm.rect.y-self.option.h*0.05),
+                         (self.worm.rect.x-self.option.w*0.02+self.option.w*0.08, self.worm.rect.y - self.option.h*0.05), int(self.option.h*0.02))
+        pygame.draw.line(self.screen, (0, 150, 0), (self.worm.rect.x - self.option.w * 0.02, self.worm.rect.y - self.option.h * 0.05),
+                         (self.worm.rect.x-self.option.w*0.02 + self.option.w*0.08*(self.health/self.max_health), self.worm.rect.y - self.option.h * 0.05), int(self.option.h * 0.02))
     def cursor(self):
         """affichage du curseur pour viser"""
         pygame.draw.circle(self.screen, (150, 0, 0), (self.viser_x,self.viser_y),
-                         5, int(self.h * 0.02))
-    def affichage_final(self):
-        self.screen.blit(self.worm.image, (self.worm.rect.x,self.worm.rect.y))
-        self.affichage_arme()
-        self.update_health_bar()
-        self.cursor()
+                         5, int(self.option.h * 0.02))
 
-        i=0
-        while i<len(self.T_fleche):
-            self.T_fleche[i].update(self.option)
-            if self.T_fleche[i].t>3:
-                del(self.T_fleche[i])
-            else:
-                i+=1
+
+
 
 
